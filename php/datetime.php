44ae82b54ddc912f44b5adb9eadf181d1b2a6b43
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-19 01:50:11 +0900

    add php

---
+++ php/datetime.php
@@ -0,0 +1,24 @@
+<?php
+$raw = '22. 11. 1968';
+$start = \DateTime::createFromFormat('d. m. Y', $raw);
+
+echo 'Start date: ' . $start->format('Y-m-d') . "\n";
+
+$end = clone $start;
+$end->add(new \DateInterval('P1M6D'));
+
+$diff = $end->diff($start);
+echo 'Difference: ' . $diff->format('%m month, %d days (total: %a days)') . "\n";
+// Difference: 1 month, 6 days (total: 37 days)
+
+if ($start < $end) {
+    echo "Start is befor end!\n";
+}
+
+$periodInterval = \DateInterval::createFromDateString('first thursday');
+$periodIterator = new \DatePeriod($start, $periodInterval, $end, \DatePeriod::EXCLUDE_START_DATE);
+foreach ($periodIterator as $date) {
+    // display every thursday
+    echo $date->format('Y-m-d') . ' ';
+}
+echo "\n";
