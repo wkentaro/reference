cff6d81d3f297dacb7a17c34251f6d62aed1ba61
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-23 22:50:06 +0900

    add triangle class

--- python/opengl/sample.py
+++ python/opengl/sample.py
@@ -3,25 +3,12 @@
 # sample.py
 # author: Kentaro Wada <www.kentaro.wada@gmail.com>
 
-from OpenGL.GL import *
-
 import glut_ui
 import glbase
 import baseview
-
-# drawing triangle
-class Triangle(object):
-    def __init__(self):
-        pass
-
-    def draw(self):
-        glBegin(GL_TRIANGLES)
-        glVertex(-1, -1) # left down
-        glVertex(1, -1)  # right down
-        glVertex(0, 1)   # up
-        glEnd()
+import triangle
 
 if __name__=="__main__":
     glut_ui.run(glbase.BaseController(
         baseview.BaseView(), 
-        Triangle()))
+        triangle.Triangle()))
