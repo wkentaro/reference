2fc0f245bf8754e7fc3b8831a9376eef88e79d1b
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-23 23:43:22 +0900

    add vertexarray.py and its sample

--- python/opengl/rotation.py
+++ python/opengl/rotation.py
@@ -13,19 +13,22 @@ import baseview
 class Rotation(baseview.BaseView):
     def __init__(self):
         super(Rotation, self).__init__()
-        self.head = 0
+        self.head_x = 0
+        self.head_y = 0
 
     def onMotion(self, x, y):
         # 回転角度
-        self.head += (x - self.x)
+        self.head_x += (x - self.x)
+        self.head_y += (y - self.y)
         self.x = x
         self.y = y
         # 再描画したいのでTrueを返す
         return True
 
     def updateView(self):
-        glRotate(self.head, 0, 1, 0)
-        print("==> GL_MODELVIEW_MATRIX {0}".format(self.head))
+        glRotate(self.head_x, 0, 1, 0)
+        glRotate(self.head_y, 1, 0, 0)
+        print("==> GL_MODELVIEW_MATRIX {0} {1}".format(self.head_x, self.head_y))
         print(glGetFloatv(GL_MODELVIEW_MATRIX))
 
 
