f265ac1e2458b1b2bca7e0495323bd8d453d2676
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-24 15:10:57 +0900

    add perspective.py

--- python/opengl/baseview.py
+++ python/opengl/baseview.py
@@ -51,10 +51,3 @@ class BaseView(object):
 
     def onMotion(self, x, y):
         print "onMotion", x, y
-
-    def printMatrix(self, m):
-        print(m[0][0], m[0][1], m[0][2], m[0][3])
-        print(m[1][0], m[1][1], m[1][2], m[1][3])
-        print(m[2][0], m[2][1], m[2][2], m[2][3])
-        print(m[3][0], m[3][1], m[3][2], m[3][3])
-
