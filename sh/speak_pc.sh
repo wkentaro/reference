b825b7eac8c5b0105c116d07b1a2107e86becfa8
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-26 00:57:09 +0900

    Update to remove no need audio file

--- sh/speak_pc.sh
+++ sh/speak_pc.sh
@@ -4,5 +4,10 @@
 wget -q -U Mozilla -O audio.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=hello+world"
 # or
 # curl -A "Mozilla" "http://translate.google.com/translate_tts?tl=en&q=hello+world" > audio.mp3
+mpg123 -q audio.mp3
+rm -f audio.mp3
 
-mpg123 audio.mp3
+
+wget -q -U Mozilla -O audio.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=ja&q=こんにちは"
+mpg123 -q audio.mp3
+rm -f audio.mp3
