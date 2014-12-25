f56a937334bd1a4dca84fa63d4473b010d3cce93
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-26 01:42:06 +0900

    update speak_pc.sh with Chinese and Japanese speaings

--- sh/speak_pc.sh
+++ sh/speak_pc.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env sh
 # speak_pc.sh
 
-wget -q -U Mozilla -O audio.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=hello+world"
+# English
+wget -q -U Mozilla -O audio.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=I+love+you."
 # or
 # curl -A "Mozilla" "http://translate.google.com/translate_tts?tl=en&q=hello+world" > audio.mp3
 mpg123 -q audio.mp3
 rm -f audio.mp3
 
+# Japanese
+wget -q -U Mozilla -O audio.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=ja&q=愛しています。"
+mpg123 -q audio.mp3
+rm -f audio.mp3
 
-wget -q -U Mozilla -O audio.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=ja&q=こんにちは"
+
+# Chinese
+wget -q -U Mozilla -O audio.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=zh&q=我爱你。"
 mpg123 -q audio.mp3
 rm -f audio.mp3
