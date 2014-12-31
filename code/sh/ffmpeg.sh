22960df878f366815f85fe867eed8de85fa6e0d7
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-31 12:25:22 +0900

    Update ffmpeg

--- code/sh/ffmpeg.sh
+++ code/sh/ffmpeg.sh
@@ -1,1 +1,11 @@
-ffmpeg -i runelena.mp4 runelena.mp3
+#!/usr/bin/env sh
+
+echo """
+$ # mp4 -> mp3
+$ ffmpeg -i audio.mp4 audio.mp3
+"""
+
+echo """
+$ # mp3 -> wav
+$ ffmpeg -i tam-n11.mp3 -ar 8000 -ac 1 tam-n11.wav
+"""
